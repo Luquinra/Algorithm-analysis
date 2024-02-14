1ccbe56a2a21c24250b81fa8fe135d489d5eccff
Luisa Quintero <82912597+Luquinra@users.noreply.github.com>
2024-02-14 08:14:01 -0500

    Update main.py

--- Reto 3/main.py
+++ Reto 3/main.py
@@ -1,6 +1,6 @@
 from math import gcd
 
-def sum_of_multiples(limit):
+def problem_1(limit):
   total_sum = 0
   for i in range(limit):
       if i % 3 == 0 or i % 5 == 0:
@@ -8,10 +8,10 @@ def sum_of_multiples(limit):
   return total_sum
 
 limit = 1000
-result = sum_of_multiples(limit)
+result = problem_1(limit)
 print("The sum of all the multiples of 3 or 5 below", limit, "is:", result)
 
-def fibonacci_sum_even(max_limit):
+def problem_2(max_limit):
   prev_term = 1
   current_term = 2
   sum_even = 0
@@ -27,10 +27,10 @@ def fibonacci_sum_even(max_limit):
   return sum_even
 
 max_limit = 4000000
-result = fibonacci_sum_even(max_limit)
+result = problem_2(max_limit)
 print("The sum of even-valued terms in the Fibonacci sequence not exceeding", max_limit, "is:", result)
 
-def largest_prime_factor(n):
+def problem_3(n):
   factor = 2  # Comenzamos con el primer número primo
   largest_factor = 0
 
@@ -43,14 +43,14 @@ def largest_prime_factor(n):
   return largest_factor
 
 number = 600851475143
-result = largest_prime_factor(number)
+result = problem_3(number)
 print("The largest prime factor of", number, "is:", result)
 
 def is_palindrome(n):
   # Convierte el número a una cadena y verifica si es igual a su reverso
   return str(n) == str(n)[::-1]
 
-def largest_palindrome_product():
+def problem_4():
   largest_palindrome = 0
   for i in range(100, 1000):
       for j in range(100, 1000):
@@ -59,7 +59,7 @@ def largest_palindrome_product():
               largest_palindrome = product
   return largest_palindrome
 
-result = largest_palindrome_product()
+result = problem_4()
 print("The largest palindrome made from the product of two 3-digit numbers is:", result)
 
 # Función para calcular el mínimo común múltiplo (LCM) de dos números
@@ -67,13 +67,13 @@ def lcm(a, b):
     return a * b // gcd(a, b)
 
 # Función para calcular el mínimo común múltiplo (LCM) de una lista de números
-def lcm_of_list(numbers):
+def problem_5(numbers):
     result = 1
     for num in numbers:
         result = lcm(result, num)
     return result
 
 # Calcula el mínimo común múltiplo (LCM) de los números del 1 al 20
-result = lcm_of_list(range(1, 21))
+result = problem_5(range(1, 21))
 
 print("The smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is:", result)
